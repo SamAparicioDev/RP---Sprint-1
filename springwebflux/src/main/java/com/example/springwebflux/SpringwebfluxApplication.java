2029461bff418de8e8e9c9cc6caa2c6ede7ac573
SamAparicioDev <samapariciodev@gmail.com>
2024-08-27 19:38:44 -0500

    se actualiza dependencia de swagger

--- springwebflux/src/main/java/com/example/springwebflux/SpringwebfluxApplication.java
+++ springwebflux/src/main/java/com/example/springwebflux/SpringwebfluxApplication.java
@@ -1,10 +1,13 @@
 package com.example.springwebflux;
 
 
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class SpringwebfluxApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringwebfluxApplication.class, args);
